2e12e2f3cb82c941b24782c1eb9ac840e80391ec
Guilherme Padilha <gs.padilha@gmail.com>
2023-04-15 07:21:38 -0300

    feat: componente Post

--- src/components/Post/index.tsx
+++ src/components/Post/index.tsx
@@ -1,7 +1,37 @@
+import styles from './Post.module.css'
+
 interface IPostProps {
   text: string
 }
 
 export function Post({ text }: IPostProps) {
-  return <p>{text}</p>
+  return (
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img
+            className={styles.avatar}
+            src="https://github.com/gspadilha.png"
+            alt=""
+          />
+          <div className={styles.authorInfo}>
+            <strong>Guilherme Padilha</strong>
+            <span>Developer</span>
+          </div>
+        </div>
+
+        <time dateTime="2022-04-15 07:17">Publicado a 1h</time>
+      </header>
+
+      <div className={styles.content}>
+        <p>Bom dia!</p>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptas
+          molestias sunt placeat delectus, modi repellendus est facere atque
+          numquam fugit minima at quod sit vero cupiditate laborum assumenda
+          nesciunt omnis!
+        </p>
+      </div>
+    </article>
+  )
 }
